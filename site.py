ef03504188415fa477cf2be909337bd3dfb5e0e2
yadiiiig <willemteughels@gmail.com>
2020-06-12 17:24:54 +0200

    setup

---
+++ site.py
@@ -0,0 +1,26 @@
+from browser import document, html
+from browser.html import AUDIO, SOURCE
+import javascript
+
+sounds = ['sound1.wav', 'sound2.wav', 'sound3.wav', 'sound4.wav', 'sound5.wav', 'sound6.wav', 'sound7.wav', 'sound8.wav']
+keys = {'49': 'pad0', '50': 'pad1', '51': 'pad2', '52': 'pad3', '53': 'pad4', '54': 'pad5', '55': 'pad6', '56': 'pad7', '57': 'pad8'}
+
+def load_sounds():
+    count = 0
+    for sound in sounds:
+        audioTag = AUDIO(id = 'pad' + str(count))
+        audioTag <= SOURCE(src = 'sounds/' + sound, )
+        document <= audioTag
+        count += 1
+
+def charCode(ev):
+    trace = document["traceCharCode"]
+    char = chr(ev.charCode)
+
+    try:
+        document[keys[str(ev.charCode)]].play()
+    except:
+        pass
+
+load_sounds()
+document["charCode"].bind("keypress", charCode)
